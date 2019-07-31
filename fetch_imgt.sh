b455e804b40a292626e42497576bb6835471d13c
mikessh <mikhail.shugay@gmail.com>
2019-07-31 19:19:36 +0300

    add imgt import script

---
+++ fetch_imgt.sh
@@ -0,0 +1,3 @@
+wget http://www.imgt.org/download/V-QUEST/IMGT_V-QUEST_reference_directory.zip
+unzip IMGT_V-QUEST_reference_directory.zip
+cat IMGT_V-QUEST_reference_directory/*/*/*.fasta > imgt_all.fasta
